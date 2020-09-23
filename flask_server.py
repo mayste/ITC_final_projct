c7e1e7c1d857ade86a9694f2d38017ea6202dc31
May Steinfeld <maylevsteinfeld@gmail.com>
2020-09-23 17:10:42 +0300

    add download from google drive

--- flask_server.py
+++ flask_server.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from transformers import TFBertForSequenceClassification
 from transformers import BertTokenizer
 from scipy.special import softmax
+from google_drive_downloader import GoogleDriveDownloader as gdd
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ def home():
 def predict_t():
     model = TFBertForSequenceClassification.from_pretrained('bert-base-uncased')
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    model.load_weights("model.h5")
+    model.load_weights('model.h5')
     message = request.form['message']
     tokenized = tokenizer(message)
     input_names = ['input_ids', 'token_type_ids', 'attention_mask']
@@ -33,6 +34,8 @@ def predict_t():
 # new_tweet = "I hate black people, they are stupid and look like shit"
 
 if __name__ == "__main__":
+    gdd.download_file_from_google_drive(file_id='1sM9RNmt4kxvyX97SxB1Zv-WMkjSxBpqH',
+                                        dest_path='./model.h5')
     port = os.environ.get('PORT')
     if port:
         app.run(host='0.0.0.0', port=int(port))
